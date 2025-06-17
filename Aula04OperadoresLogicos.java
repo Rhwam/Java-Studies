f5fe0a3d570b8c8e3402a6e5bfc0cdfd236fa862
Rhwam <rhwamteixeira2@hotmail.com>
2025-06-17 19:56:52 -0300

    Operadores lógicos

--- Aula04OperadoresLogicos.java
+++ Aula04OperadoresLogicos.java
@@ -1,4 +1,13 @@
 package DevDojo.Iniciando;
 
 public class Aula04OperadoresLogicos {
+    public static void main(String[] args) {
+        // && (AND) || (OR) !
+        int age = 35;
+        float salary = 3500F;
+        boolean isLegalGreater = age > 30 && salary >= 4612;
+        boolean isLegalMinor = age < 30 && salary >= 3381;
+        System.out.println(isLegalGreater);
+        System.out.println(isLegalMinor);
+    }
 }
